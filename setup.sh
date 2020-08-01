946255df4e1460508794e1af772c4ab1b543f110
extremecoders-re <extremecoders@hotmail.com>
2020-08-01 22:43:34 +0530

    Add set -e

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -e
 sudo apt update
 sudo apt install -y python-pip python3-pip python3-pexpect unzip busybox-static fakeroot kpartx snmp uml-utilities util-linux vlan qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
 
