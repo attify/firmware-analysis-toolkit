49940256c15e5b2a0ca0ef36d34748b0028aadca
Decidedly Gray <decidedlygray@users.noreply.github.com>
2021-04-07 16:19:00 -0500

    Updated dependencies
    Updated outdated dependencies

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 set -e
 sudo apt update
-sudo apt install -y python-pip python3-pip python3-pexpect unzip busybox-static fakeroot kpartx snmp uml-utilities util-linux vlan qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
+sudo apt install -y python3-pip python3-pexpect unzip busybox-static fakeroot kpartx snmp uml-utilities util-linux vlan qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
 
 echo "Installing binwalk"
-git clone --depth=1 https://github.com/devttys0/binwalk.git
+git clone --depth=1 https://github.com/ReFirmLabs/binwalk.git
 cd binwalk
 sudo ./deps.sh
 sudo python3 ./setup.py install
